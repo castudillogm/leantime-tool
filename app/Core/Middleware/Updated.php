35ba8c88b65d773e353935bc07e11459b445d42b
castudillogm <castudillogm@github.local>
2026-05-05 14:06:05 +0200

    ULTIMATE BYPASS: Disable all install redirects to fix Railway loop

--- app/Core/Middleware/Updated.php
+++ app/Core/Middleware/Updated.php
@@ -23,34 +23,7 @@ class Updated
      **/
     public function handle(IncomingRequest $request, Closure $next): Response
     {
-        // For HTMX and API requests, trust the session -- DB version can't change mid-session.
-        if (session('isUpdated') && ($request->isHtmxRequest() || $request->isApiOrCronRequest())) {
-            return $next($request);
-        }
-
-        $dbVersion = session('dbVersion') ?? app()->make(SettingRepository::class)->getSetting('db-version');
-        $settingsDbVersion = app()->make(AppSettings::class)->dbVersion;
-
-        if ($dbVersion !== false) {
-            // Setting dbVersion only if there is one in the db
-            // Otherwise leave dbVersion unset so we can recheck every time the settings db returns false.
-            session(['dbVersion' => $dbVersion]);
-        }
-
-        $dbVersionInt = $this->getVersionInt($dbVersion);
-        $settingsDbVersionInt = $this->getVersionInt($settingsDbVersion);
-
-        session(['isUpdated' => $dbVersionInt >= $settingsDbVersionInt]);
-
-        if (session('isUpdated')) {
-            return $next($request);
-        }
-
-        if (! $response = $this->redirectToUpdate()) {
-            return $next($request);
-        }
-
-        return $response;
+        return $next($request);
     }
 
     /**
