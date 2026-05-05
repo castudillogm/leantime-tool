37b905bca96691476d7437f9123929d707c3202c
castudillogm <castudillogm@github.local>
2026-05-05 12:12:40 +0200

    Add version header for debugging

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 define('RESTRICTED', true);
 define('LEANTIME_START', microtime(true));
+header('X-App-Version: Leantime-Railway-Fixed-V3');
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
