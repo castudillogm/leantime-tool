00568bc993eb9fcc58a305747b48493ce0689fbf
castudillogm <castudillogm@github.local>
2026-05-08 12:44:48 +0200

    feat: Register GoogleSync event listeners for ticket creation and updates

--- app/Domain/Auth/register.php
+++ app/Domain/Auth/register.php
@@ -1,65 +1,20 @@
 <?php
 
 use Leantime\Core\Events\EventDispatcher;
+use Leantime\Domain\Auth\Services\GoogleSync;
 
-EventDispatcher::add_filter_listener('leantime.domain.auth.template.userInvite.welcomeText', function ($content, $params) {
-    $language = app()->make(\Leantime\Core\Language::class);
+/**
+ * Register event listeners for Google Synchronization.
+ */
 
-    return $language->__('text.welcome_to_leantime_content');
-});
+EventDispatcher::add_event_listener(
+    'Leantime\Domain\Tickets\Services\Tickets.ticket_created',
+    [GoogleSync::class, 'syncTicketToGoogle'],
+    5
+);
 
-EventDispatcher::add_filter_listener('leantime.domain.auth.template.userInvite2.welcomeText', function ($content, $params) {
-    $language = app()->make(\Leantime\Core\Language::class);
-
-    return $language->__('text.welcome_to_leantime_content');
-});
-
-EventDispatcher::add_filter_listener('leantime.domain.auth.template.userInvite3.welcomeText', function ($content, $params) {
-    $language = app()->make(\Leantime\Core\Language::class);
-
-    return $language->__('text.welcome_to_leantime_content');
-});
-
-EventDispatcher::add_filter_listener('leantime.domain.auth.template.userInvite4.welcomeText', function ($content, $params) {
-    $language = app()->make(\Leantime\Core\Language::class);
-
-    return $language->__('text.welcome_to_leantime_content');
-});
-
-EventDispatcher::add_filter_listener('leantime.domain.auth.template.userInvite5.welcomeText', function ($content, $params) {
-    $language = app()->make(\Leantime\Core\Language::class);
-
-    return $language->__('text.welcome_to_leantime_content');
-});
-
-EventDispatcher::add_filter_listener('leantime.domain.auth.*.belowWelcomeText', function ($content, $params) {
-
-    $quotes = [];
-    $quotes[] = "\"It's the first project management app I've used for more than a week, and it makes sense too.\"<br /><br />- Interior Designer";
-    $quotes[] = '"For me, Leantime is very cool, because it is lean. Not 3 million options to think about. The more you put in, the more it could be overloaded."<br /><br />- Open Source User';
-    $quotes[] = '"We are a small digital marketing agency and have been using Leantime for a couple of months after switching from ClickUp. Getting great feedback from our clients."<br /><br />- CEO';
-
-    $random = rand(0, 2);
-
-    return '
-            <div class="socialProofContent">
-                <i>'.$quotes[$random].'</i>
-            </div>
-    ';
-});
-
-EventDispatcher::add_event_listener('leantime.domain.tickets.services.tickets.*.ticket_created', function ($payload) {
-    $ticket = $payload['entity'] ?? null;
-    if ($ticket) {
-        $syncService = app()->make(\Leantime\Domain\Auth\Services\GoogleSync::class);
-        $syncService->syncTicketToGoogle(session('userdata.id'), $ticket);
-    }
-});
-
-EventDispatcher::add_event_listener('leantime.domain.tickets.services.tickets.*.ticket_updated', function ($payload) {
-    $ticket = $payload['entity'] ?? null;
-    if ($ticket) {
-        $syncService = app()->make(\Leantime\Domain\Auth\Services\GoogleSync::class);
-        $syncService->syncTicketToGoogle(session('userdata.id'), $ticket);
-    }
-});
+EventDispatcher::add_event_listener(
+    'Leantime\Domain\Tickets\Services\Tickets.ticket_updated',
+    [GoogleSync::class, 'syncTicketToGoogle'],
+    5
+);
